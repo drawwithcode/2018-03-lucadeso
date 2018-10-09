a6ba316e2ad39cfd16a086c96c79d58476a1d4d0
lucadeso <lukedesogus@gmail.com>
2018-10-09 20:13:40 +0200

    es3_texture

--- sketch.js
+++ sketch.js
@@ -3,9 +3,44 @@ function preload(){
 }
 
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight);
+  background(0);
+  pixelDensity(0.5);
+  blendMode(OVERLAY);
 }
 
 function draw() {
-  // put drawing code here
+  // translate(mouseX, mouseY);
+
+  scale(-500,-500);
+  var xoff = 0;
+
+  loadPixels();
+  for (var x = 0; x < width; x++) {
+    var yoff = 0;
+    for (var y = 0; y < height; y++) {
+      var index = (x + y * width) * 4;
+      var r = noise(xoff, yoff) * (mouseX/3+mouseY/5);
+      if (mouseX>750) {
+      pixels[index] = mouseY/100+r/2;
+      pixels[index+1] = r/20+mouseX/200;
+      pixels[index+2] = r+mouseY/20;
+      pixels[index+3] = r+(mouseX+mouseY)/3;
+    } else {
+      pixels[index] = mouseY/10+r/2;
+      pixels[index+1] = r/2+mouseY/20;
+      pixels[index+2] = cos(mouseX/200);
+      pixels[index+3] = r;
+    }
+
+
+      yoff += tan(0.05);
+    }
+    xoff += 0.01;
+
+
+  }
+  updatePixels();
+  // noLoop();
+
 }
